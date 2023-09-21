5f0df8bc2d3f9fdf11f7bb942414513e7dfd473b
harshpathakzz <harshpathak2017@gmail.com>
2023-09-21 20:16:15 +0530

    updated

--- chat.js
+++ chat.js
@@ -1,19 +1,20 @@
-import { config } from "dotenv";
+import { config } from "dotenv"; // Load environment variables from a .env file
 config();
 
-import { ConversationChain } from "langchain/chains";
-import { ChatOpenAI } from "langchain/chat_models/openai";
+import { ConversationChain } from "langchain/chains"; // Import ConversationChain class
+import { ChatOpenAI } from "langchain/chat_models/openai"; // Import ChatOpenAI class
 import {
   ChatPromptTemplate,
   HumanMessagePromptTemplate,
   SystemMessagePromptTemplate,
   MessagesPlaceholder,
-} from "langchain/prompts";
+} from "langchain/prompts"; // Import various templates for creating chat prompts
 
-import { BufferMemory } from "langchain/memory";
-import chalk from "chalk";
-import inquirer from "inquirer"; // Import inquirer library
+import { BufferMemory } from "langchain/memory"; // Import BufferMemory for conversation history
+import chalk from "chalk"; // Import chalk for colorful console output
+import inquirer from "inquirer"; // Import inquirer for user input
 
+// Initialize the ChatOpenAI model with specific parameters
 const chat = new ChatOpenAI({
   temperature: 0.7,
   maxTokens: 50,
@@ -21,7 +22,7 @@ const chat = new ChatOpenAI({
   presencePenalty: 1,
 });
 
-// Define personality object with references to movies and literature
+// Define personalities and their associated responses
 const personality = {
   oogway:
     "I am Oogway, an old turtle from the Valley of Peace, known for my wisdom in the 'Kung Fu Panda' series. I speak in proverbs and riddles.",
@@ -29,58 +30,62 @@ const personality = {
     "I am Rafiki, the wise baboon from Disney's 'The Lion King.' Ask me about life lessons, and I'll share my wisdom. Remember, 'The past can hurt, but the way I see it, you can either run from it or learn from it.'",
   shakespeare:
     "I am William Shakespeare, the famous playwright. Seek my counsel on matters of literature, love, and the human condition. As I once wrote, 'All the world's a stage, and all the men and women merely players.'",
-  // Add more personalities with references and prompts here
 };
 
+// Define colorful styles for console output
 const colorfulStyles = {
-  title: chalk.hex("#13b541"), // Blue text on yellow background
-  userPrompt: chalk.hex("#00FFFF"), // Cyan color for user prompts
-  characterName: chalk.hex("##00FFFF"), // Magenta for character names
-  response: chalk.hex("#E2894B"), // Green for responses
+  title: chalk.hex("#13b541"), // Title color
+  userPrompt: chalk.hex("#00FFFF"), // User's input color
+  characterName: chalk.hex("##00FFFF"), // Character's name color
+  response: chalk.hex("#E2894B"), // Response color
 };
 
-// Function to create a chat prompt from a personality
+// Function to create a chat prompt based on the selected personality
 function createChatPrompt(personName) {
   return ChatPromptTemplate.fromPromptMessages([
     SystemMessagePromptTemplate.fromTemplate(
       colorfulStyles.characterName(personality[personName])
-    ), // Use magenta color
+    ),
     new MessagesPlaceholder("history"),
     HumanMessagePromptTemplate.fromTemplate(
       colorfulStyles.userPrompt("{input}")
-    ), // Use cyan color
+    ),
   ]);
 }
 
+// Main function
 async function main() {
-  console.log(colorfulStyles.title("\nWisdomGPT\n")); // Use colorful title
+  console.log(colorfulStyles.title("\nWisdomGPT\n")); // Display a title
 
-  const selectedPersonality = await choosePersonality(); // Choose personality dynamically
+  const selectedPersonality = await choosePersonality(); // Allow the user to choose a personality
 
+  // Create a ConversationChain with memory, prompt, and the ChatOpenAI model
   const chain = new ConversationChain({
     memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
-    prompt: createChatPrompt(selectedPersonality), // Use the selected personality
+    prompt: createChatPrompt(selectedPersonality),
     llm: chat,
   });
 
   let input;
 
   do {
-    input = await askQuestion();
+    input = await askQuestion(); // Ask the user for input
 
-    const wait = waitAnimation();
+    const wait = waitAnimation(); // Start a loading animation
 
-    const response = await chain.call({ input });
+    const response = await chain.call({ input }); // Make a conversation call to the model
 
-    wait.stop();
+    wait.stop(); // Stop the loading animation
 
+    // Display the character's name and response in specified colors
     console.log(
-      colorfulStyles.characterName(`${selectedPersonality}: `) + // Display the character's name in magenta
-        colorfulStyles.response(response.response) // Display the response in green
+      colorfulStyles.characterName(`${selectedPersonality}: `) +
+        colorfulStyles.response(response.response)
     );
-  } while (input !== "exit");
+  } while (input !== "exit"); // Continue the loop until the user types "exit"
 }
 
+// Function to create a loading animation
 function waitAnimation() {
   let interval;
   const frames = ["-", "\\", "|", "/"];
@@ -99,28 +104,30 @@ function waitAnimation() {
   };
 }
 
+// Function to ask the user for input
 async function askQuestion() {
   const { userInput } = await inquirer.prompt([
     {
       type: "input",
       name: "userInput",
-      message: colorfulStyles.userPrompt("You: "), // Use cyan color for the user input prompt
+      message: colorfulStyles.userPrompt("You: "), // Prompt the user with "You: "
     },
   ]);
   return userInput;
 }
 
+// Function to allow the user to choose a personality
 async function choosePersonality() {
   const personalityChoices = Object.keys(personality);
   const { selectedPersonality } = await inquirer.prompt([
     {
       type: "list",
       name: "selectedPersonality",
-      message: colorfulStyles.userPrompt("Choose a personality:"), // Use cyan color for the prompt
+      message: colorfulStyles.userPrompt("Choose a personality:"), // Prompt the user to choose a personality
       choices: personalityChoices,
     },
   ]);
   return selectedPersonality;
 }
 
-main();
+main(); // Call the main function to start the program
